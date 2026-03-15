87fdd6e94479233fc6389a5bf4c835384f125c61
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2026-03-15 14:47:45 +0300

    Final version
    Ready for Review

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <algorithm>
+#include <iostream>
 
 template <class T>
 struct List{
   T val; List<T>* next;
-}
+};
 
 template <class T>
 struct Vec{
   T* data; size_t s, cap;
-}
+};
 
 template <class T>
-void clear(Vec<List<T>*> v){
-  for(size_t i = 0; i < s; ++i){
+void clear(Vec<List<T>*>& v){
+  for(size_t i = 0; i < v.s; ++i){
     while (v.data[i] != nullptr){
       List<T>* n = v.data[i]->next;
       delete v.data[i];
@@ -22,46 +23,49 @@ void clear(Vec<List<T>*> v){
 }
 
 template <class T>
-size_t count (Vect<List<T>*> v){
+size_t count (Vec<List<T>*> v){
   size_t ans = 0;
-  for(size_t i = 0; i < v.s; ++i){
-    while(v.data[i] != nullptr) {
-      ans++;
-      v.data[i] = v.data[i[->next;
-    }
-  }
+  for (size_t i = 0; i < v.s; ++i) for (List<T>* p = v.data[i]; p; p = p->next) ++ans;
   return ans;
 }
 
 template <class T>
-List<T>* getNext(Vec<List<T>*> v, size_t& pos){
-  if(v.data[pos]->next != nullptr){
-    v.data[pos] = v.data[pos]->next;
-    return v.data[pos];
+List<T>* getNext(Vec<List<T>*>& v, size_t& pos){
+  if(v.data[pos] != nullptr){
+    List<T>* tmp = v.data[pos];
+    v.data[pos] = tmp->next;
+    return tmp;
   }
-  while (v.data[++pos] == nullptr) {}
-  return v.data[pos];
+  while (pos < v.s && v.data[++pos] == nullptr) {}
+  if (pos != v.s) return v.data[pos];
+  else return nullptr;
 }
 
 template <class T>
 Vec<List<T>*> balanced_move(Vec<List<T>*> v, size_t k){
-  Vec<List<T>*> ans = Vec(new T[v.cap], v.s, v.cap);
+  Vec<List<T>*> ans = Vec(new List<T>*[v.cap], v.s, v.cap);
   Vec<List<T>*> e = v;
   size_t pos = 0;
-  for(size_t i = 0; i < std::max(count(e)/k, ans.cap); ++i){
-    ans.data[i] = getNext(Vec<List<T>*> e, pos);
-    List<T>* tmp = ans.data[i];
-    for(size_t j = 1; j < k; ++j){
-      tmp = tmp->next;
-      tmp = getNext(Vec<List<T>*> e, pos);
+  try{
+    for(size_t i = 0; i < std::min(count(e)/k, ans.cap); ++i){
+      ans.data[i] = getNext(e, pos);
+      List<T>* tmp = ans.data[i];
+      for(size_t j = 1; j < k; ++j){
+        tmp->next = getNext(e, pos);
+        tmp = tmp->next;
+      }
+      tmp->next = nullptr;
     }
-    tmp->next = nullptr;
-  }
-  ans.data[ans.cap - 1] = getNext(Vec<List<T>*> e, pos);
-  List<T>* tmp = ans.data[i];
-  while (e.data[pos] != nullptr){
-    tmp = tmp->next;
-    tmp = getNext(Vec<List<T>*> e, pos);
+    ans.data[ans.cap - 1] = getNext(e, pos);
+    List<T>* tmp = ans.data[ans.cap - 1];
+    while (e.data[pos] != nullptr){
+      tmp->next = getNext(e, pos);
+      tmp = tmp->next;
+    }
+  } catch(...){
+    clear(ans);
+    std::cerr << "Balanced move faliure\n";
+    throw;
   }
   return ans;
 }
